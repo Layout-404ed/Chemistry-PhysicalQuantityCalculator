ba9bdf503eeb535f95ca9238ef448182e198c289
Layout_404ed <layout.568.2017@gmail.com>
2025-12-20 12:40:50 -0500

    slight edits to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 // Physical Quantity Calculator
+#define 
 #include "PhysicalQuantity.hpp"
 #include "PhysicalQuantity.cpp"
-
+void input();
+void display();
 int main(void)
 {
-    PhysicalQuantity quantity();
+    PhysicalQuantity quantity;
+
     std::cout << "Physical Quantity Calculator\n"
               << "____________________________\n";
     //TODO: create input and display method
+    int x = 0;
 
     return 0;
+}
+
+void input(){
+
+}
+void output(){
+
 }
